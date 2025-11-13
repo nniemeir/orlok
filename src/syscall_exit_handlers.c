eb769b2c0feca6890fbf9496d6f52612fbcb5d2e
Jacob Niemeir <nniemeir@protonmail.com>
2025-11-12 22:47:34 -0600

    Cleaned up errno handling for syscalls

--- src/syscall_exit_handlers.c
+++ src/syscall_exit_handlers.c
@@ -9,6 +9,16 @@
 #include "syscall_types.h"
 #include "trace.h"
 
+// Raw syscalls return -errno on error (e.g. -2 for ENOENT)
+// Their glibc wrappers return -1 on error and set errno
+void print_errno(unsigned long long rax) {
+  if ((long)rax < 0) {
+    printf("(%s)\n", strerror(-(int)rax));
+  } else {
+    printf("\n");
+  }
+}
+
 void handle_read_exit(pid_t pid, struct user_regs_struct *regs,
                       syscalls_state *state) {
   state->read_values.buffer = malloc(ARG_MAX);
@@ -18,15 +28,10 @@ void handle_read_exit(pid_t pid, struct user_regs_struct *regs,
   }
   read_string_arg(&state->read_values.buffer, pid, regs->rsi);
   state->read_values.ret_value = (long)regs->rax;
-
   printf("read(%u, \"%s\", %zu) = %ld ", state->read_values.fd,
          state->read_values.buffer, state->read_values.count,
          state->read_values.ret_value);
-  if (state->read_values.ret_value < 0) {
-    printf("(%s)\n", strerror(-regs->rax));
-  } else {
-    printf("\n");
-  }
+  print_errno(regs->rax);
   free(state->read_values.buffer);
 }
 
@@ -34,13 +39,10 @@ void handle_write_exit(pid_t pid, struct user_regs_struct *regs,
                        syscalls_state *state) {
   (void)pid;
   state->write_values.ret_value = (long)regs->rax;
-  state->write_values.errno_value = "N/A";
-  if (state->write_values.ret_value < 0) {
-    state->write_values.errno_value = strerror(-regs->rax);
-  }
-  printf("write(%u, \"%s\", %zu) = %ld (%s)\n", state->write_values.fd,
+  printf("write(%u, \"%s\", %zu) = %ld ", state->write_values.fd,
          state->write_values.buffer, state->write_values.count,
-         state->write_values.ret_value, state->write_values.errno_value);
+         state->write_values.ret_value);
+  print_errno(regs->rax);
   free(state->write_values.buffer);
 }
 
@@ -48,26 +50,19 @@ void handle_close_exit(pid_t pid, struct user_regs_struct *regs,
                        syscalls_state *state) {
   (void)pid;
   state->close_values.ret_value = (long)regs->rax;
-  state->close_values.errno_value = "N/A";
-  if (state->close_values.ret_value < 0) {
-    state->close_values.errno_value = strerror(-regs->rax);
-  }
-  printf("close(%u) = %ld\n", state->close_values.fd,
+  printf("close(%u) = %ld ", state->close_values.fd,
          state->close_values.ret_value);
+  print_errno(regs->rax);
 }
 
 void handle_openat_exit(pid_t pid, struct user_regs_struct *regs,
                         syscalls_state *state) {
   (void)pid;
   state->openat_values.ret_value = (int)regs->rax;
-  state->openat_values.errno_value = "N/A";
-  if (state->openat_values.ret_value < 0) {
-    state->openat_values.errno_value = strerror(-regs->rax);
-  }
-  printf("openat(%u, \"%s\", %d, %u) = %d (%s)\n", state->openat_values.dfd,
+  printf("openat(%u, \"%s\", %d, %u) = %d ", state->openat_values.dfd,
          state->openat_values.filename, state->openat_values.flags,
-         state->openat_values.mode, state->openat_values.ret_value,
-         state->openat_values.errno_value);
+         state->openat_values.mode, state->openat_values.ret_value);
+  print_errno(regs->rax);
   free(state->openat_values.filename);
 }
 
@@ -75,54 +70,38 @@ void handle_lseek_exit(pid_t pid, struct user_regs_struct *regs,
                        syscalls_state *state) {
   (void)pid;
   state->lseek_values.ret_value = (off_t)regs->rax;
-  state->lseek_values.errno_value = "N/A";
-  if (state->lseek_values.ret_value < 0) {
-    state->lseek_values.errno_value = strerror(-regs->rax);
-  }
-  printf("lseek(%u, %ld, %d) = %ld (%s)\n", state->lseek_values.fd,
+  printf("lseek(%u, %ld, %d) = %ld ", state->lseek_values.fd,
          state->lseek_values.offset, state->lseek_values.whence,
-         state->lseek_values.ret_value, state->lseek_values.errno_value);
+         state->lseek_values.ret_value);
+  print_errno(regs->rax);
 }
 
 void handle_brk_exit(pid_t pid, struct user_regs_struct *regs,
                      syscalls_state *state) {
   (void)pid;
   state->brk_values.ret_value = (int)regs->rax;
-  state->brk_values.errno_value = "N/A";
-  if (state->brk_values.ret_value < 0) {
-    state->brk_values.errno_value = strerror(-regs->rax);
-  }
-  printf("brk(%lu) = %d (%s)\n", state->brk_values.addr,
-         state->brk_values.ret_value, state->brk_values.errno_value);
+  printf("brk(%lu) = %d ", state->brk_values.addr, state->brk_values.ret_value);
+  print_errno(regs->rax);
 }
 
 void handle_mmap_exit(pid_t pid, struct user_regs_struct *regs,
                       syscalls_state *state) {
   (void)pid;
   state->mmap_values.ret_value = (void *)regs->rax;
-  state->mmap_values.errno_value = "N/A";
-  if (state->mmap_values.ret_value == (void *)-1) {
-    state->mmap_values.errno_value =
-        strerror(-(long)state->mmap_values.ret_value);
-  }
-  printf("mmap(%lu, %lu, %d, %d, %d, %ld) = %p (%s)\n", state->mmap_values.addr,
+  printf("mmap(%lu, %lu, %d, %d, %d, %ld) = %p ", state->mmap_values.addr,
          state->mmap_values.length, state->mmap_values.prot,
          state->mmap_values.flags, state->mmap_values.fd,
-         state->mmap_values.offset, state->mmap_values.ret_value,
-         state->mmap_values.errno_value);
+         state->mmap_values.offset, state->mmap_values.ret_value);
+  print_errno(regs->rax);
 }
 
 void handle_munmap_exit(pid_t pid, struct user_regs_struct *regs,
                         syscalls_state *state) {
   (void)pid;
   state->munmap_values.ret_value = (int)regs->rax;
-  state->munmap_values.errno_value = "N/A";
-  if (state->munmap_values.ret_value < 0) {
-    state->munmap_values.errno_value = strerror(-regs->rax);
-  }
-  printf("munmap(%lu, %lu) = %d (%s)\n", state->munmap_values.addr,
-         state->munmap_values.length, state->munmap_values.ret_value,
-         state->munmap_values.errno_value);
+  printf("munmap(%lu, %lu) = %d ", state->munmap_values.addr,
+         state->munmap_values.length, state->munmap_values.ret_value);
+  print_errno(regs->rax);
 }
 
 void handle_exit_exit(pid_t pid, struct user_regs_struct *regs,
@@ -150,39 +129,29 @@ void handle_fork_exit(pid_t pid, struct user_regs_struct *regs,
                       syscalls_state *state) {
   (void)pid;
   state->fork_values.ret_value = (pid_t)regs->rax;
-  state->fork_values.errno_value = "N/A";
-  if (state->fork_values.ret_value < 0) {
-    state->fork_values.errno_value = strerror(-regs->rax);
-  }
-  printf("fork() = %d (%s)\n", state->fork_values.ret_value,
-         state->fork_values.errno_value);
+  printf("fork() = %d ", state->fork_values.ret_value);
+  print_errno(regs->rax);
 }
 
 void handle_clone_exit(pid_t pid, struct user_regs_struct *regs,
                        syscalls_state *state) {
   (void)pid;
   state->clone_values.ret_value = (long)regs->rax;
-  state->clone_values.errno_value = "N/A";
-  if (state->clone_values.ret_value < 0) {
-    state->clone_values.errno_value = strerror(-regs->rax);
-  }
   printf("clone(%lu, %p, %d, %d, %lu) = %ld (%s)\n", state->clone_values.flags,
          state->clone_values.stack, state->clone_values.parent_tid,
          state->clone_values.child_tid, state->clone_values.tls,
          state->clone_values.ret_value, state->clone_values.errno_value);
+  print_errno(regs->rax);
 }
 
 void handle_execve_exit(pid_t pid, struct user_regs_struct *regs,
                         syscalls_state *state) {
   (void)pid;
   state->execve_values.ret_value = (int)regs->rax;
-  state->execve_values.errno_value = "N/A";
-  if (state->execve_values.ret_value < 0) {
-    state->execve_values.errno_value = strerror(-regs->rax);
-  }
-  printf("execve(\"%s\", %s, %s) = %d (%s)\n", state->execve_values.pathname,
+  printf("execve(\"%s\", %s, %s) = %d ", state->execve_values.pathname,
          state->execve_values.argv, state->execve_values.envp,
-         state->execve_values.ret_value, state->execve_values.errno_value);
+         state->execve_values.ret_value);
+  print_errno(regs->rax);
   free(state->execve_values.pathname);
 }
 
@@ -191,19 +160,14 @@ void handle_stat_exit(pid_t pid, struct user_regs_struct *regs,
   (void)pid;
   state->stat_values.statbuf = (void *)regs->rsi;
   state->stat_values.ret_value = (int)regs->rax;
-  state->stat_values.errno_value = "N/A";
-  if (state->stat_values.ret_value < 0) {
-    state->stat_values.errno_value = strerror(-regs->rax);
-  }
   if (regs->orig_rax == SYS_stat) {
-    printf("stat(\"%s\", %p) = %d (%s)\n", state->stat_values.filename,
-           state->stat_values.statbuf, state->stat_values.ret_value,
-           state->stat_values.errno_value);
+    printf("stat(\"%s\", %p) = %d ", state->stat_values.filename,
+           state->stat_values.statbuf, state->stat_values.ret_value);
   } else {
-    printf("lstat(\"%s\", %p) = %d (%s)\n", state->stat_values.filename,
-           state->stat_values.statbuf, state->stat_values.ret_value,
-           state->stat_values.errno_value);
+    printf("lstat(\"%s\", %p) = %d ", state->stat_values.filename,
+           state->stat_values.statbuf, state->stat_values.ret_value);
   }
+  print_errno(regs->rax);
   free(state->stat_values.filename);
 }
 
@@ -212,26 +176,18 @@ void handle_fstat_exit(pid_t pid, struct user_regs_struct *regs,
   (void)pid;
   state->fstat_values.statbuf = (void *)regs->rsi;
   state->fstat_values.ret_value = (int)regs->rax;
-  state->fstat_values.errno_value = "N/A";
-  if (state->fstat_values.ret_value < 0) {
-    state->fstat_values.errno_value = strerror(-regs->rax);
-  }
-  printf("fstat(%d, %p) = %d (%s)\n", state->fstat_values.fd,
-         state->fstat_values.statbuf, state->fstat_values.ret_value,
-         state->fstat_values.errno_value);
+  printf("fstat(%d, %p) = %d ", state->fstat_values.fd,
+         state->fstat_values.statbuf, state->fstat_values.ret_value);
+  print_errno(regs->rax);
 }
 
 void handle_access_exit(pid_t pid, struct user_regs_struct *regs,
                         syscalls_state *state) {
   (void)pid;
   state->access_values.ret_value = (int)regs->rax;
-  state->access_values.errno_value = "N/A";
-  if (state->access_values.ret_value < 0) {
-    state->access_values.errno_value = strerror(-regs->rax);
-  }
-  printf("access(\"%s\", %d) = %d (%s)\n", state->access_values.pathname,
-         state->access_values.mode, state->access_values.ret_value,
-         state->access_values.errno_value);
+  printf("access(\"%s\", %d) = %d ", state->access_values.pathname,
+         state->access_values.mode, state->access_values.ret_value);
+  print_errno(regs->rax);
   free(state->access_values.pathname);
 }
 
@@ -249,13 +205,9 @@ void handle_getcwd_exit(pid_t pid, struct user_regs_struct *regs,
     exit(1);
   }
   read_string_arg(&state->getcwd_values.ret_value, pid, regs->rax);
-  state->getcwd_values.errno_value = "N/A";
-  if (state->getcwd_values.ret_value == NULL) {
-    state->getcwd_values.errno_value = strerror(-regs->rax);
-  }
-  printf("getcwd(\"%s\", %lu) = %s (%s)\n", state->getcwd_values.buf,
-         state->getcwd_values.size, state->getcwd_values.ret_value,
-         state->getcwd_values.errno_value);
+  printf("getcwd(\"%s\", %lu) = %s ", state->getcwd_values.buf,
+         state->getcwd_values.size, state->getcwd_values.ret_value);
+  print_errno(regs->rax);
   free(state->getcwd_values.buf);
   free(state->getcwd_values.ret_value);
 }
@@ -264,12 +216,9 @@ void handle_chdir_exit(pid_t pid, struct user_regs_struct *regs,
                        syscalls_state *state) {
   (void)pid;
   state->chdir_values.ret_value = (int)regs->rax;
-  state->chdir_values.errno_value = "N/A";
-  if (state->chdir_values.ret_value < 0) {
-    state->chdir_values.errno_value = strerror(-regs->rax);
-  }
-  printf("chdir(\"%s\") = %d, (%s)\n", state->chdir_values.path,
-         state->chdir_values.ret_value, state->chdir_values.errno_value);
+  printf("chdir(\"%s\") = %d ", state->chdir_values.path,
+         state->chdir_values.ret_value);
+  print_errno(regs->rax);
   free(state->chdir_values.path);
 }
 
@@ -277,58 +226,45 @@ void handle_socket_exit(pid_t pid, struct user_regs_struct *regs,
                         syscalls_state *state) {
   (void)pid;
   state->socket_values.ret_value = (int)regs->rax;
-  state->socket_values.errno_value = "N/A";
-  if (state->socket_values.ret_value < 0) {
-    state->socket_values.errno_value = strerror(-regs->rax);
-  }
-  printf("socket(%d, %d, %d) = %d, (%s)\n", state->socket_values.domain,
+  printf("socket(%d, %d, %d) = %d ", state->socket_values.domain,
          state->socket_values.type, state->socket_values.protocol,
-         state->socket_values.ret_value, state->socket_values.errno_value);
+         state->socket_values.ret_value);
+  print_errno(regs->rax);
 }
 
 void handle_bind_exit(pid_t pid, struct user_regs_struct *regs,
                       syscalls_state *state) {
   (void)pid;
   state->bind_values.ret_value = (int)regs->rax;
-  state->bind_values.errno_value = "N/A";
-  if (state->bind_values.ret_value < 0) {
-    state->bind_values.errno_value = strerror(-regs->rax);
-  }
-  printf("bind(%d, %p, %u) = %d (%s)\n", state->bind_values.sockfd,
+  printf("bind(%d, %p, %u) = %d ", state->bind_values.sockfd,
          state->bind_values.addr, state->bind_values.addrlen,
-         state->bind_values.ret_value, state->bind_values.errno_value);
+         state->bind_values.ret_value);
+  print_errno(regs->rax);
 }
 
 void handle_listen_exit(pid_t pid, struct user_regs_struct *regs,
                         syscalls_state *state) {
   (void)pid;
   state->listen_values.ret_value = (int)regs->rax;
-  state->listen_values.errno_value = "N/A";
-  if (state->listen_values.ret_value < 0) {
-    state->listen_values.errno_value = strerror(-regs->rax);
-  }
-  printf("listen(%d, %d) = %d (%s)\n", state->listen_values.sockfd,
-         state->listen_values.backlog, state->listen_values.ret_value,
-         state->listen_values.errno_value);
+  printf("listen(%d, %d) = %d ", state->listen_values.sockfd,
+         state->listen_values.backlog, state->listen_values.ret_value);
+  print_errno(regs->rax);
 }
 
 void handle_accept_exit(pid_t pid, struct user_regs_struct *regs,
                         syscalls_state *state) {
   (void)pid;
   state->accept_values.ret_value = (int)regs->rax;
-  state->accept_values.errno_value = "N/A";
-  if (state->accept_values.ret_value < 0) {
-    state->accept_values.errno_value = strerror(-regs->rax);
-  }
   if (regs->orig_rax == SYS_accept) {
-    printf("accept(%d, %p, %u) = %d (%s)", state->accept_values.sockfd,
+    printf("accept(%d, %p, %u) = %d ", state->accept_values.sockfd,
            state->accept_values.addr, state->accept_values.addrlen,
-           state->accept_values.ret_value, state->accept_values.errno_value);
+           state->accept_values.ret_value);
   } else {
-    printf("connect(%d, %p, %u) = %d (%s)", state->accept_values.sockfd,
+    printf("connect(%d, %p, %u) = %d ", state->accept_values.sockfd,
            state->accept_values.addr, state->accept_values.addrlen,
-           state->accept_values.ret_value, state->accept_values.errno_value);
+           state->accept_values.ret_value);
   }
+  print_errno(regs->rax);
 }
 
 void handle_pipe_exit(pid_t pid, struct user_regs_struct *regs,
@@ -346,36 +282,24 @@ void handle_pipe_exit(pid_t pid, struct user_regs_struct *regs,
     fprintf(stderr, "PTRACE_PEEKDATA Failed: %s\n", strerror(errno));
   }
   state->pipe_values.ret_value = (int)regs->rax;
-  state->pipe_values.errno_value = "N/A";
-  if (state->pipe_values.ret_value < 0) {
-    state->pipe_values.errno_value = strerror(-regs->rax);
-  }
-  printf("pipe([%d, %d]) = %d (%s)", state->pipe_values.pipefd[0],
-         state->pipe_values.pipefd[1], state->pipe_values.ret_value,
-         state->pipe_values.errno_value);
+  printf("pipe([%d, %d]) = %d ", state->pipe_values.pipefd[0],
+         state->pipe_values.pipefd[1], state->pipe_values.ret_value);
+  print_errno(regs->rax);
 }
 
 void handle_dup_exit(pid_t pid, struct user_regs_struct *regs,
                      syscalls_state *state) {
   (void)pid;
   state->dup_values.ret_value = (int)regs->rax;
-  state->dup_values.errno_value = "N/A";
-  if (state->dup_values.ret_value < 0) {
-    state->dup_values.errno_value = strerror(-regs->rax);
-  }
-  printf("dup(%d) = %d (%s)", state->dup_values.oldfd,
-         state->dup_values.ret_value, state->dup_values.errno_value);
+  printf("dup(%d) = %d ", state->dup_values.oldfd, state->dup_values.ret_value);
+  print_errno(regs->rax);
 }
 
 void handle_dup2_exit(pid_t pid, struct user_regs_struct *regs,
                       syscalls_state *state) {
   (void)pid;
   state->dup2_values.ret_value = (int)regs->rax;
-  state->dup2_values.errno_value = "N/A";
-  if (state->dup2_values.ret_value < 0) {
-    state->dup2_values.errno_value = strerror(-regs->rax);
-  }
-  printf("dup2(%d, %d) = %d (%s)", state->dup2_values.oldfd,
-         state->dup2_values.newfd, state->dup2_values.ret_value,
-         state->dup2_values.errno_value);
+  printf("dup2(%d, %d) = %d ", state->dup2_values.oldfd,
+         state->dup2_values.newfd, state->dup2_values.ret_value);
+  print_errno(regs->rax);
 }
